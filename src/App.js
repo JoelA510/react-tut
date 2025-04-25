65c787577737792be34e1dd7d5a19deaeefc6173
JoelA510 <joela510@gmail.com>
2025-04-25 12:58:40 -0700

    Error Fix and Video 6 of tutorial

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from './home';
 function App() {
   return (
     <div className="App">
-      <Navbar />
+        <Navbar />
       <div className="content">
         <Home />
       </div>
