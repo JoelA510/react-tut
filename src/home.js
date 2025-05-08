2e0c572693e321716a0687a181a8928a058fc4ee
JoelA510 <joela510@gmail.com>
2025-05-08 09:44:31 -0700

    Video 16: Using JSON Server

--- src/home.js
+++ src/home.js
@@ -8,23 +8,18 @@ const Home = () => {
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
   ]);
 
-  const [name, setName] = useState('mario');
-
-  const handleDelete = (id) => {
+   const handleDelete = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
     setBlogs(newBlogs);
   }
 
   useEffect(() => {
     console.log('use effect ran');
-    console.log(name);
-  }, [name]);
+  }, []);
 
     return ( 
       <div className="home">
         <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
-      <button onClick={() => setName('luigi')}>change name</button>
-      <p>{ name }</p>
       </div>
     );
 }
