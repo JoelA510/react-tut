07533d78ce7e7355fcf7b02ab4df7c0c93fcfee4
JoelA510 <joela510@gmail.com>
2025-05-15 13:34:06 -0700

    Video 21: React Router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 
 import Navbar from './navbar';
 import Home from './home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <div className="App">
         <Navbar />
-      <div className="content">
-        <Home />
+          <div className="content">
+            <Switch>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
